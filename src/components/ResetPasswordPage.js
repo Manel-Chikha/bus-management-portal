e84ebc8c4ae3b75bd23d5ab34bc5c6743483a5de
Manel-Chikha <Manelchikha67@gmail.com>
2025-06-03 20:57:35 +0100

    Update ResetPasswordPage.js

--- src/components/ResetPasswordPage.js
+++ src/components/ResetPasswordPage.js
@@ -3,7 +3,7 @@ import { useSearchParams, useNavigate } from 'react-router-dom';
 import { FiLock, FiCheck, FiX, FiEye, FiEyeOff } from 'react-icons/fi';
 import './ResetPasswordPage.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL || 'https://nfc-application-latest-4.onrender.com';
 
 const ResetPasswordPage = () => {
   const [searchParams] = useSearchParams();
